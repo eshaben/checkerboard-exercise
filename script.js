c581670c0ed759cb63c5b3803654c7302e93ad97
Erin Shaben <eshaben@icloud.com>
2017-06-18 22:15:13 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 // Your JS goes here
 
-for (var i=0; i<36; i++){
-var createDiv = document.createElement('div')
-createDiv.style.cssText = 'width:11.1%; height: 10vw; float:left; paddingBottom: 11.1%; background: black;'
-console.log(createDiv)
-document.body.appendChild(createDiv)
+for (var i = 0; i < 36; i++) {
+  var createDiv = document.createElement('div')
+  createDiv.style.cssText = 'width:11.1%; height: 10vw; float:left; paddingBottom: 11.1%; background: black;'
+  console.log(createDiv)
+  document.body.appendChild(createDiv)
 
-var createDiv = document.createElement('div')
-createDiv.style.cssText = 'width:11.1%; height: 10vw; float:left; paddingBottom: 11.1%; background: red;'
-console.log(createDiv)
-document.body.appendChild(createDiv)
+  var createDiv = document.createElement('div')
+  createDiv.style.cssText = 'width:11.1%; height: 10vw; float:left; paddingBottom: 11.1%; background: red;'
+  console.log(createDiv)
+  document.body.appendChild(createDiv)
 }
-
-
-// selectDiv.style.width = '11.1%';
-// selectDiv.style['float'] = 'left';
-// selectDiv.style['paddingBottom'] = '11.1%';
-// selectDiv.style['background'] = 'black';
